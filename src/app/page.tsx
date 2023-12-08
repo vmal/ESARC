49c3f0e8aa610c8e86ad9ebe78b1f8a45c039c22
vaibhav malhotra <vaibhavmalhotra@vaibhavs-Laptop.hitronhub.home>
2023-12-07 18:34:45 -0800

    project files

---
+++ src/app/page.tsx
@@ -0,0 +1,221 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { ContactSection } from '@/components/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import { List, ListItem } from '@/components/List'
+import { SectionIntro } from '@/components/SectionIntro'
+import { StylizedImage } from '@/components/StylizedImage'
+import { Testimonial } from '@/components/Testimonial'
+import logoBrightPath from '@/images/clients/bright-path/logo-light.svg'
+import logoFamilyFund from '@/images/clients/family-fund/logo-light.svg'
+import logoGreenLife from '@/images/clients/green-life/logo-light.svg'
+import logoHomeWork from '@/images/clients/home-work/logo-light.svg'
+import logoMailSmirk from '@/images/clients/mail-smirk/logo-light.svg'
+import logoNorthAdventures from '@/images/clients/north-adventures/logo-light.svg'
+import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
+import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
+import logoUnseal from '@/images/clients/unseal/logo-light.svg'
+import imageLaptop from '@/images/laptop.jpg'
+import { type CaseStudy, type MDXEntry, loadCaseStudies } from '@/lib/mdx'
+
+const clients = [
+  ['Phobia', logoPhobiaLight],
+  ['Family Fund', logoFamilyFund],
+  ['Unseal', logoUnseal],
+  ['Mail Smirk', logoMailSmirk],
+  ['Home Work', logoHomeWork],
+  ['Green Life', logoGreenLife],
+  ['Bright Path', logoBrightPath],
+  ['North Adventures', logoNorthAdventures],
+]
+
+function Clients() {
+  return (
+    <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
+      <Container>
+        <FadeIn className="flex items-center gap-x-8">
+          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+            We’ve worked with hundreds of amazing people
+          </h2>
+          <div className="h-px flex-auto bg-neutral-800" />
+        </FadeIn>
+        <FadeInStagger faster>
+          <ul
+            role="list"
+            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4"
+          >
+            {clients.map(([client, logo]) => (
+              <li key={client}>
+                <FadeIn>
+                  <Image src={logo} alt={client} unoptimized />
+                </FadeIn>
+              </li>
+            ))}
+          </ul>
+        </FadeInStagger>
+      </Container>
+    </div>
+  )
+}
+
+function CaseStudies({
+  caseStudies,
+}: {
+  caseStudies: Array<MDXEntry<CaseStudy>>
+}) {
+  return (
+    <>
+      <SectionIntro
+        title="Harnessing technology for a brighter future"
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          We believe technology is the answer to the world’s greatest
+          challenges. It’s also the cause, so we find ourselves in bit of a
+          catch 22 situation.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+                <h3>
+                  <Link href={caseStudy.href}>
+                    <span className="absolute inset-0 rounded-3xl" />
+                    <Image
+                      src={caseStudy.logo}
+                      alt={caseStudy.client}
+                      className="h-16 w-16"
+                      unoptimized
+                    />
+                  </Link>
+                </h3>
+                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                  <time
+                    dateTime={caseStudy.date.split('-')[0]}
+                    className="font-semibold"
+                  >
+                    {caseStudy.date.split('-')[0]}
+                  </time>
+                  <span className="text-neutral-300" aria-hidden="true">
+                    /
+                  </span>
+                  <span>Case study</span>
+                </p>
+                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
+                  {caseStudy.title}
+                </p>
+                <p className="mt-4 text-base text-neutral-600">
+                  {caseStudy.description}
+                </p>
+              </article>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+      </Container>
+    </>
+  )
+}
+
+function Services() {
+  return (
+    <>
+      <SectionIntro
+        eyebrow="Services"
+        title="Empowering Innovations, Building Futures"
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          Your vision, whether a startup concept, a small business digital upgrade,
+          or a corporate innovation, deserves a crafted technological foundation.
+          We are here to bring your ideas to life.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <div className="lg:flex lg:items-center lg:justify-end">
+          <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
+            <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
+              <StylizedImage
+                src={imageLaptop}
+                sizes="(min-width: 1024px) 41rem, 31rem"
+                className="justify-center lg:justify-end"
+              />
+            </FadeIn>
+          </div>
+          <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
+            <ListItem title="Web/Mobile App development">
+              Elevate your online presence with our integrated web and mobile app development services. We build responsive,
+              user-centric applications for all platforms, using cutting-edge technologies like Next.js, React for web apps,
+              and delivering seamless mobile experiences on iOS and Android.
+              From initial design to final deployment, our development process is streamlined to bring
+              your business to the forefront of digital innovation.
+            </ListItem>
+            <ListItem title="AI & Machine Learning Integration">
+              Step into the future with our AI development services. We design sophisticated chatbots,
+              implement computer vision, and create personalized AI solutions using the latest technologies from
+              OpenAI, Google Cloud, and AWS. Whether its enhancing user interaction through intelligent conversational agents
+              or providing deep insights with advanced analytics, our AI services are tailored to elevate your
+              business in the smartest way possible.
+            </ListItem>
+            <ListItem title="MVP and Prototyping">
+              Transform your startup idea into a tangible product with our MVP and prototyping services.
+              We help you test the waters with a lean yet impactful version of your product, ready to scale.
+            </ListItem>
+            <ListItem title="Legacy Repurposing">
+              Your outdated systems hold untapped potential.
+              We specialize in modernizing and repurposing existing projects,
+              giving them a new lease on life and aligning them with current market demands.
+            </ListItem>
+          </List>
+        </div>
+      </Container>
+    </>
+  )
+}
+
+export const metadata: Metadata = {
+  description:
+    'We are a development studio working at the intersection of design and technology.',
+}
+
+export default async function Home() {
+  let caseStudies = (await loadCaseStudies()).slice(0, 3)
+
+  return (
+    <>
+      <Container className="mt-24 sm:mt-32 md:mt-56">
+        <FadeIn className="max-w-3xl">
+          <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+            Award-winning software development agency based in Vancouver.
+          </h1>
+          <p className="mt-6 text-xl text-neutral-600">
+            We are a forward-thinking development agency where design meets innovation.
+            At the heart of the tech revolution, our dedicated team crafts
+            digital solutions that resonate with your audience and stand the test of time.
+          </p>
+        </FadeIn>
+      </Container>
+
+      <Clients />
+
+      <CaseStudies caseStudies={caseStudies} />
+
+      <Testimonial
+        className="mt-24 sm:mt-32 lg:mt-40"
+        client={{ name: 'Phobia', logo: logoPhobiaDark }}
+      >
+        We were thoroughly impressed with how ESARC transformed our customer service portal. Their integration of an AI-powered chatbot that
+        intelligently categorizes customer queries and suggests solutions has drastically improved our response times.
+        The fact that it was implemented with such a natural user interface, avoiding any cumbersome installation or complex navigation for users, was nothing short of remarkable.
+      </Testimonial>
+
+      <Services />
+
+      <ContactSection />
+    </>
+  )
+}
---
+++ src/components/Offices.tsx
@@ -0,0 +1,50 @@
+import clsx from 'clsx'
+
+function Office({
+  name,
+  children,
+  invert = false,
+}: {
+  name: string
+  children: React.ReactNode
+  invert?: boolean
+}) {
+  return (
+    <address
+      className={clsx(
+        'text-sm not-italic',
+        invert ? 'text-neutral-300' : 'text-neutral-600',
+      )}
+    >
+      <strong className={invert ? 'text-white' : 'text-neutral-950'}>
+        {name}
+      </strong>
+      <br />
+      {children}
+    </address>
+  )
+}
+
+export function Offices({
+  invert = false,
+  ...props
+}: React.ComponentPropsWithoutRef<'ul'> & { invert?: boolean }) {
+  return (
+    <ul role="list" {...props}>
+      <li>
+        <Office name="Vancouver" invert={invert}>
+          1668 Davie Street
+          <br />
+          Vancouver, BC
+        </Office>
+      </li>
+      <li>
+        <Office name="Toronto" invert={invert}>
+          Don Mills Road
+          <br />
+          Toronto, ON
+        </Office>
+      </li>
+    </ul>
+  )
+}
